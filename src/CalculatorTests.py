9051c700cc7b86ac93dc612cb01932a92bd92255
Sakthisundar Arivudainambi <sakthisundar2207@gmail.com>
2020-11-01 17:14:18 +0530

    Feature: Added Subtraction method to CalculatorTest

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -17,6 +17,12 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
+    def test_sub_method_calculator(self):
+        test_data = CsvReader("src/Unit Test Subtraction.csv").data
+        for row in test_data:
+            self.assertEqual(self.calculator.sub(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
 
 if __name__ == '__main__':
     unittest.main()
