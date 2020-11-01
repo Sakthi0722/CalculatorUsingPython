a840859f5538ac802495cc163fc00466d3f43f0c
Sakthisundar Arivudainambi <sakthisundar2207@gmail.com>
2020-11-01 18:09:19 +0530

    Fix: changed round value

--- src/Calculator.py
+++ src/Calculator.py
@@ -68,5 +68,5 @@ class Calculator:
         return self.result
 
     def square_root(self, a):
-        self.result = round(sqrt(a), 9)
+        self.result = round(sqrt(a), 11)
         return self.result
