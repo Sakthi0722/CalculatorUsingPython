5f89bc985dbb2a8649bbe0bf4509a287beb2eeb4
Sakthisundar Arivudainambi <sakthisundar2207@gmail.com>
2020-11-01 17:13:55 +0530

    Feature: Added Subtraction method to Calculator

--- src/Calculator.py
+++ src/Calculator.py
@@ -5,6 +5,13 @@ def addition(a, b):
     return c
 
 
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
 class Calculator:
     result = 0
 
@@ -13,4 +20,8 @@ class Calculator:
 
     def add(self, a, b):
         self.result = addition(a, b)
+        return self.result
+
+    def sub(self, a, b):
+        self.result = subtraction(a, b)
         return self.result
