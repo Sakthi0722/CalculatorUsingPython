4470a00199defa4c45b3e9f8a194872647f22c4e
Sakthisundar Arivudainambi <sakthisundar2207@gmail.com>
2020-11-01 17:05:27 +0530

    Feature: Added Addition method to CalculatorTest

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,9 +1,21 @@
 import unittest
+from Calculator import Calculator
+from CsvReader import CsvReader
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_add_method_calculator(self):
+        test_data = CsvReader("src/Unit Test Addition.csv").data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
 
 if __name__ == '__main__':
