ae851951b7df0c1ccc114bcad2edc1dfc8fa0c70
Sakthisundar Arivudainambi <sakthisundar2207@gmail.com>
2020-11-01 18:04:46 +0530

    Feature: Added Square root method to CalculatorTest

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -41,6 +41,12 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.squares(row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
+    def test_sqrt_method_calculator(self):
+        test_data = CsvReader("src/Unit Test Square Root.csv").data
+        for row in test_data:
+            self.assertEqual(self.calculator.square_root(row['Value 1']), float(row['Result']))
+            self.assertEqual(self.calculator.result, float(row['Result']))
+
 
 if __name__ == '__main__':
     unittest.main()
